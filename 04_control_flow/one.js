ecaf0de7769bf28611250ede0fe0c0ea08d29f70
Tanishq Gupta <tanishqgupta8311@gmail.com>
2024-10-05 16:51:00 +0530

    added control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+// if
+// if(2==2) {
+//     console.log('if');
+// }
+
+// if(2=="2"){
+//     console.log('if');
+// }
+// if(2==="2"){ //strict equality
+//     console.log('else if');
+// }
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power: ${power}`);   
+// }
+
+
+// const balance = 1000
+// if(balance>500){
+//     console.log('balance is greater than 500'); 
+// }else{
+//     console.log('balance is less than 500');
+// }   
+
+const userLoggedIn = true
+const debitCard = true
+if(userLoggedIn && debitCard){
+    console.log('you can purchase');
+}   
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,36 @@
+// const userEmail=[]
+// if(userEmail){
+//     console.log("Email is present");
+// }
+// else{
+//     console.log("Email is not present");
+// }
+
+/**
+ * falsy values- false, 0, "", null, undefined, NaN,BigInt 0n
+ * truthy values- true, 1, "a", {}, [], function(){}
+ */
+// if(userEmail.length===0){
+//     console.log("Email is not present");
+// }
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("Object is empty");
+// }
+
+// nullish coalescing operator (??) : null and undefined
+// used for default values or checking null or undefined
+// let val1
+// val1=5 ?? 10
+// val1=null ?? 10
+// val1=10 ?? undefined
+// val1=null ?? undefined
+// val1=null?? 10 ?? 20
+// console.log(val1);
+
+// ternianry operator
+// condition ? true : false
+// const iiceTeaPrice=100
+// iiceTeaPrice >=80 ? console.log("i will buy ice tea") : console.log("i will not buy ice tea")
+
